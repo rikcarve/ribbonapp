2201679d2ff284d11762862251613a553d6babcc
rikcarve <rik@carve.ch>
2015-09-30 21:04:41 +0200

    intgrated CuratorServiceLocator into CuratorServiceList

--- src/main/java/carve/ribbonapp/CuratorServerList.java
+++ src/main/java/carve/ribbonapp/CuratorServerList.java
@@ -1,14 +1,44 @@
 package carve.ribbonapp;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
 
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractServerList;
 import com.netflix.loadbalancer.Server;
 
+/**
+ * Ribbon server list which uses Curator to lookup available servers. Must be
+ * specified through the property "NIWSServerListClassName"
+ */
 public class CuratorServerList extends AbstractServerList<Server> {
 
+    private static ServiceDiscovery<Object> serviceDiscovery;
     private String clientName;
+
+    static {
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(
+                "localhost:2181", new RetryNTimes(5, 1000));
+        curatorFramework.start();
+
+        serviceDiscovery = ServiceDiscoveryBuilder
+                .builder(Object.class).basePath("carve")
+                .client(curatorFramework).build();
+        try {
+            serviceDiscovery.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Curator init complete");
+    }
 
     @Override
     public List<Server> getInitialListOfServers() {
@@ -19,7 +49,7 @@ public class CuratorServerList extends AbstractServerList<Server> {
     @Override
     public List<Server> getUpdatedListOfServers() {
         System.out.println("getUpdatedListOfServers for " + clientName);
-        return CuratorServiceLocator.getListOfServers(clientName);
+        return getListOfServers(clientName);
         // List<Server> list = new ArrayList<Server>();
         // list.add(new Server("localhost", 8180));
         // return list;
@@ -28,6 +58,19 @@ public class CuratorServerList extends AbstractServerList<Server> {
     @Override
     public void initWithNiwsConfig(IClientConfig clientConfig) {
         clientName = clientConfig.getClientName();
+    }
+
+    private List<Server> getListOfServers(String serviceName) {
+        List<Server> list = new ArrayList<Server>();
+        try {
+            Collection<ServiceInstance<Object>> instances = serviceDiscovery.queryForInstances(serviceName);
+            for (ServiceInstance<Object> instance : instances) {
+                list.add(new Server(instance.getAddress(), instance.getPort()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
 }
--- src/main/java/carve/ribbonapp/GreetingService.java
+++ src/main/java/carve/ribbonapp/GreetingService.java
@@ -9,7 +9,10 @@ import com.netflix.ribbon.proxy.annotation.Http;
 import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 
-@ClientProperties(properties = { @Property(name = "ReadTimeout", value = "2000"), @Property(name = "ConnectTimeout", value = "1000"), @Property(name = "NIWSServerListClassName", value = "carve.ribbonapp.CuratorServerList") })
+@ClientProperties(properties = {
+        @Property(name = "ReadTimeout", value = "2000"),
+        @Property(name = "ConnectTimeout", value = "1000"),
+        @Property(name = "NIWSServerListClassName", value = "carve.ribbonapp.CuratorServerList") })
 public interface GreetingService {
 
     @Http(method = HttpMethod.GET, uri = "/carve.greeting/v1/greeting/")
